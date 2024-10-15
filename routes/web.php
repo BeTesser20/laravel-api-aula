54cca2923dba1cce1b5a6471646e81d71b5072c1
BeTesser20 <beatrizptesser@gmail.com>
2024-10-15 09:20:09 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,18 @@ Route::get('/', function () {
         'message' => 'OK'
     ]);
 });
+
+Route::get('/somar', function(Request $request) {
+    // Não está chegando nada pela request
+    if(count($request->all()) < 1) {
+        return response()->json([
+            'message' => 'Não há valores para somar'
+        ], 406);
+    }
+
+    $soma = array_sum($request->all());
+    return response()->json([
+        'message' => 'somado com sucesso',
+        'sum' => $soma
+    ]);
+});
